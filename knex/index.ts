d740dfa912802ec85f8f5560f7299743ab691856
Dominic Smith <dominic.smith@loke.com.au>
2019-02-18 14:10:46 +1100

    Add setup function

--- knex/index.ts
+++ knex/index.ts
@@ -3,6 +3,7 @@ import decamelize from "decamelize";
 import path from "path";
 import { format as formatUrl } from "url";
 import redactor from "url-auth-redactor";
+import Knex from "knex";
 
 type Connection =
   | string
@@ -18,6 +19,11 @@ interface Options {
   databaseName: string;
   migrationsDirectory?: string;
   client?: string;
+}
+
+interface Logger {
+  info(...msg: string[]): void;
+  error(...msg: string[]): void;
 }
 
 type StringTransform = (str: string) => string;
@@ -56,6 +62,35 @@ export function createConfig(opts: Options) {
     ) => origImpl(decamelize(value))
   };
 }
+
+export async function setup(dbClient: Knex, logger: Logger = console) {
+  for (;;) {
+    try {
+      const [batchNo, log] = await dbClient.migrate.latest();
+      if (log.length) {
+        logger.info(`Migration batch ${batchNo} run: ${log.length} migrations`);
+        logger.info(log.join("\n"));
+      }
+      break;
+    } catch (err) {
+      switch (err.code) {
+        case "ENOTFOUND":
+        case "ECONNREFUSED":
+          logger.error("Could not connect to db:", err.code);
+          break;
+        default:
+          throw err;
+      }
+
+      await delay(3000);
+
+      logger.info("Retrying db setup...");
+    }
+  }
+}
+
+const delay = (timeout: number) =>
+  new Promise(resolve => setTimeout(resolve, timeout));
 
 export function formatConnection(connection: Connection) {
   if (typeof connection === "string") {
